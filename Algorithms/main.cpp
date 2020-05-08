d1ca522ec9ad6f489c1b63e38dea0b48f7b21635
phenixcxz <phenixcxz@163.com>
2020-05-08 10:48:21 +0800

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by cxz on 2020/5/8.
+//  Copyright © 2020 cxz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
